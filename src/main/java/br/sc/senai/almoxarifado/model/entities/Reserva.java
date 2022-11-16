1f3f4bb1ff1d89e5e0777a16306f605e5a272c07
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-11-16 18:14:51 -0300

    feat: update reserva

--- src/main/java/br/sc/senai/almoxarifado/model/entities/Reserva.java
+++ src/main/java/br/sc/senai/almoxarifado/model/entities/Reserva.java
@@ -1,11 +1,15 @@
 package br.sc.senai.almoxarifado.model.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.PastOrPresent;
 import java.util.Date;
 import java.util.List;
 
@@ -28,12 +32,15 @@ public class Reserva
     @Column(nullable = false)
     private Status status;
 
+    @Future
     @Column(length = 50, nullable = false)
     private Date dataRetirada;
 
+    @PastOrPresent
     @Column(length = 50, nullable = true)
     private Date dataRetirado;
 
+    @FutureOrPresent
     @Column(length = 50, nullable = false)
     private Date dataDevolucao;
 
